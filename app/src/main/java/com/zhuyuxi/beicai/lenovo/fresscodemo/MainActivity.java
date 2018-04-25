e52003eb60ba56fd13c567f12591eb6f1cd4d735
caicancan <18062411472@163.com>
2018-04-25 16:20:05 +0800

    cc

--- app/src/main/java/com/zhuyuxi/beicai/lenovo/fresscodemo/MainActivity.java
+++ app/src/main/java/com/zhuyuxi/beicai/lenovo/fresscodemo/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ViewUtils.inject(this);
         requestImage();
+        
 //        Uri uri = Uri.parse(img_url01);
 //        draweeView.setImageURI(uri);
 
